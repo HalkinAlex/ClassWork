0e583b6feca33a89722672facb3063f22c5764bf
Alex <158268731+HalkinAlex@users.noreply.github.com>
2024-03-06 09:03:27 +0000

    change

--- hello.py
+++ hello.py
@@ -1,1 +1,19 @@
-print ("Hello, World!")
+word = ""
+seen_characters = set()
+
+while True:
+    char = input("Введіть символ (для завершення введіть 'exit'): ")
+
+    if char == 'exit':
+        break
+
+    if char in seen_characters:
+        print(f"no sequential duplicates allowed, you’ve entered: {word}")
+        break
+
+    word += char
+    seen_characters.add(char)
+
+    if word == word[::-1]:
+        print(f"it’s palindrome: {word}")
+        break
